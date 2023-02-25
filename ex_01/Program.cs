7ae1ce6db0dfd61c3dcb0716474c8c88699e042c
DestinedByFate <maloy_bm@mail.ru>
2023-02-26 00:53:15 +0500

    add ex_01

---
+++ ex_01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+double max = 0;
+double min = 0;
+
+if (num1 > num2) 
+{
+    max = num1;
+    min = num2;
+    Console.WriteLine("Максимальное число: " + max);
+    Console.WriteLine("Минимальное число: " + min);
+}
+else 
+{
+    max = num2;
+    min = num1;
+    Console.WriteLine("Максимальное число: " + max);
+    Console.WriteLine("Минимальное число: " + min);
+}
