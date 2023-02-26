c2f8601b0087b12652a16fbd32bf366af934d613
DestinedByFate <maloy_bm@mail.ru>
2023-02-26 13:34:19 +0500

    add exercise 002 comparison

--- ex_02/Program.cs
+++ ex_02/Program.cs
@@ -1,14 +1,25 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Input first number: ");
+Console.Write("Input the first number: ");
 double num1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input second number: ");
+Console.Write("Input the second number: ");
 double num2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input third number: ");
+Console.Write("Input the third number: ");
 double num3 = Convert.ToDouble(Console.ReadLine());
-max = 0;
+double max = 0;
 
-if (num1 > num2 > num3)
+if (num1 >= num2 && num1 >= num3) 
 {
     max = num1;
+    Console.WriteLine($"Max number is {max}");
+}
+else if (max < num2 && num2 > num3)
+{
+    max = num2;
+    Console.WriteLine($"Max number is {max}");
+}
+else 
+{
+    max = num3;
+    Console.WriteLine($"Max number is {max}");
 }
