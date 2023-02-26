e0e8edef61a9a1d1de9a131999889f1faf6f8e94
DestinedByFate <maloy_bm@mail.ru>
2023-02-26 16:05:37 +0500

    add ex03 even numbers

---
+++ ex_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Input number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} - number is even");
+}
+else
+{
+    Console.WriteLine($"{number} - number is uneven");
+}
