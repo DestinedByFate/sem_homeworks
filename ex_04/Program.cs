574c7813e21ddaef1c443ddfd6964f7c0b229da9
DestinedByFate <maloy_bm@mail.ru>
2023-02-26 17:08:04 +0500

    add ex04 row of even numbers

---
+++ ex_04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+
+Console.Write("Even numbers are: ");
+
+while (count <= number)
+{
+    if (count % 2 == 0) 
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
+if (number == 0 || number == 1)
+{
+    Console.Write("none");
+}
